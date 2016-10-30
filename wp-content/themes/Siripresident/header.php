4eeeff8494410143a208bc4b7d9cea0f01cf297e
moh12594 <moh12594@gmail.com>
2016-10-30 16:09:48 +0100

    Add a viewport and mediaqueries to the headerbackground

--- wp-content/themes/Siripresident/header.php
+++ wp-content/themes/Siripresident/header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
         <?php wp_head(); ?>
     </head>
