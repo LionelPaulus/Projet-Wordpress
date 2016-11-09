9e695f08d82bbd5d6a2cd9087aaf4ff321073961
Lionel Paulus <lionelpaulus@gmail.com>
2016-11-09 18:49:35 +0100

    Dynamic CSS url test

--- wp-content/themes/Siripresident/inc/stylesadd.php
+++ wp-content/themes/Siripresident/inc/stylesadd.php
@@ -3,9 +3,9 @@ function ajout_scripts() {
 
   wp_register_script('main_script', get_template_directory_uri() . '/js/app.js', array('jquery'),'1.1', true);
   wp_enqueue_script('main_script');
-  wp_register_style( 'main_style', get_template_directory_uri() . '/css/app.css');
+  wp_register_style( 'main_style', "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."/css/app.css");
   wp_enqueue_style( 'main_style' );
-  
+
 }
 add_action( 'after_setup_theme', 'ajout_scripts' );
 
