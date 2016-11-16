f145ad0717deefb42da2852ec1d051240273aa65
Lionel Paulus <lionelpaulus@gmail.com>
2016-11-16 21:10:59 +0100

    Insert votes in database

--- wp-content/themes/Siripresident/page-templates/ajax-annonces.php
+++ wp-content/themes/Siripresident/page-templates/ajax-annonces.php
@@ -1,7 +1,16 @@
 <?php
 
-$news = $_POST['news'];
-echo $news;
+$news = htmlspecialchars($_POST['news']);
+$vote = htmlspecialchars($_POST['vote']);
+
+$wpdb->insert(
+	$wpdb->prefix.'news',
+	array(
+		'name' => $news,
+		'vote' => $vote
+	)
+);
+
 die();
 
 ?>
