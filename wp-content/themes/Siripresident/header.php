1bbf490e21c694ddf4bbacf10c2a175037ba65a6
moh12594 <moh12594@gmail.com>
2016-11-17 21:25:23 +0100

    [typo:scss] change to myriad

--- wp-content/themes/Siripresident/header.php
+++ wp-content/themes/Siripresident/header.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<script src="https://use.typekit.net/alv0dqz.js"></script>
+		<script>try{Typekit.load({ async: true });}catch(e){}</script>
 		<title>Siri Président</title>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
         <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
