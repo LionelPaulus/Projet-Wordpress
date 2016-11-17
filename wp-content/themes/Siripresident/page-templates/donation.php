2583962d2dffe1cb0286e69bb518ffa651e41595
moh12594 <moh12594@gmail.com>
2016-11-17 12:42:47 +0100

    [donation:php] Style of the page 2

--- wp-content/themes/Siripresident/page-templates/donation.php
+++ wp-content/themes/Siripresident/page-templates/donation.php
@@ -13,6 +13,7 @@ Template Name: Page de donation
 
   <div class="container">
   <h2 class="center"><?php the_title(); ?></h2>
+  <div class="row">
       <?php
     if (have_posts()){
         while (have_posts()){
@@ -23,6 +24,7 @@ Template Name: Page de donation
     }
     }
     ?>
+  </div>
   </div>
 
 <?php
