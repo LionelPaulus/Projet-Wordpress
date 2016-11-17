90540de268199611fec0160ec00850e68399173f
LouisRialland <Lo.Rialland@gmail.com>
2016-11-17 17:02:37 +0100

    Fix page programmation

--- wp-content/themes/Siripresident/page-templates/programmation.php
+++ wp-content/themes/Siripresident/page-templates/programmation.php
@@ -1,42 +1,20 @@
-<?php  //appel du template header.php
+<?php //appel du template header.php
 
 
 get_header();
 get_template_part('page-templates/header');
-$args=array(
-    'post_type' => 'post',
-    'posts_per_page' => 50,
-    'paged' => $paged,
-    'tax_query' => array(
-        array(
-            'taxonomy' => 'category',
-            'field'    => 'term_id',
-            'terms'    => $term_id,
-        ),
-    )
-);
-
-// The Query
-//$wp_query->max_num_pages
-$the_query = new WP_Query( $args );
 
 ?>
 <body>
 
 <section>
-
-
-
-
-
     <ul id="slide-out" class="side-nav already_visible">
         <?php
-        $terms = get_terms( 'category', 'hide_empty=0' );
+        $terms = get_terms('category', 'hide_empty=0');
 
-        if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+        if (!empty($terms) && !is_wp_error($terms)) {
 
-            foreach ( $terms as $term )
-            {
+            foreach ($terms as $term) {
                 echo '<li><a href="#" title="' . $term->name . '" data-id="' . $term->term_id . '">
                 ' . $term->name . '</a>
                 </li>';
@@ -44,31 +22,22 @@ $the_query = new WP_Query( $args );
 
         }
         ?>
-
     </ul>
     <div class="programmation_container">
         <div class="card-panel white accent-1 description_programmation">
             <h4>Notre Programme</h4>
             <p>Retrouvez tous les projets de campagnes que Siri mettra en place une fois élu.
-                N'hésitez pas à modifier l'emplacement des différents projets dans leurs ordre d'importance selon vous,(les plus importants en haut).</p>
+                N'hésitez pas à modifier l'emplacement des différents projets dans leurs ordre d'importance selon
+                vous,(les plus importants en haut).</p>
             <div class="article_prog">
 
             </div>
 
         </div>
     </div>
-
-
-
-
-
 </section>
-
-
 <script src="https://maps.googleapis.com/maps/api/js" type="text/javascript"></script>
-
 </body>
-
 <?php
 
 get_template_part('page-templates/footer');
