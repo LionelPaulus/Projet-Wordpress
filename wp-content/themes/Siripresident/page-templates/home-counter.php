b6deda55e1260f1081a4024968953a37493f91c5
LaurenceRouge <laurence.rouge@hetic.net>
2016-11-17 20:46:03 +0100

    [home-counter] : modification css

--- wp-content/themes/Siripresident/page-templates/home-counter.php
+++ wp-content/themes/Siripresident/page-templates/home-counter.php
@@ -4,20 +4,21 @@
       Nombres de problématiques résolues par SIRI
     </div>
   <div class="Counter">
-    <div class="Counter__unit">
-      162418
-    </div>
-  <div class="Counter__unit" data-js="counter-thousands">
-      <div class="Counter__number" data-js="current">0</div>
-      <div class="Counter__number" data-js="next">0</div>
+    <div class="Counter__number">8</div>
+    <div class="Counter__number">4</div>
+    <div class="Counter__number">1</div>
+    <div class="Counter__number">2</div>
+    <div class="Counter__unit" data-js="counter-thousands">
+        <div class="Counter__number" data-js="current">1</div>
+        <div class="Counter__number" data-js="next">1</div>
     </div>
     <div class="Counter__unit" data-js="counter-hundreds">
-      <div class="Counter__number" data-js="current">0</div>
-      <div class="Counter__number" data-js="next">0</div>
+      <div class="Counter__number" data-js="current">9</div>
+      <div class="Counter__number" data-js="next">9</div>
     </div>
   <div class="Counter__unit" data-js="counter-tens">
-    <div class="Counter__number" data-js="current">0</div>
-    <div class="Counter__number" data-js="next">0</div>
+    <div class="Counter__number" data-js="current">9</div>
+    <div class="Counter__number" data-js="next">9</div>
   </div>
   <div class="Counter__unit" data-js="counter-ones">
     <div class="Counter__number" data-js="current">0</div>
